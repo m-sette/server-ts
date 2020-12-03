fefd21ca87a685bb038a95c45c2efb44cdf68a0d
Matte Sette <msette8@gmail.com>
2020-12-03 15:07:44 +0100

    save guard route handler

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,16 +1,21 @@
 import 'reflect-metadata';
 import { Methods } from './Methods';
 import { MetadataKeys } from './MetadaKeys';
+import { RequestHandler } from 'express';
 
-function routeBinder(method: string) {
-    return function (path: string) {
-        return function (target: any, key: string, desc: PropertyDescriptor) {
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+    value?: RequestHandler;
+}
+
+const routeBinder = (method: string) => {
+    return (path: string) => {
+        return (target: any, key: string, desc: RouteHandlerDescriptor) => {
             Reflect.defineMetadata(MetadataKeys.path, path, target, key);
             Reflect.defineMetadata(MetadataKeys.method, method, target, key);
         };
     };
 
-}
+};
 export const get = routeBinder(Methods.get);
 export const put = routeBinder(Methods.put);
 export const post = routeBinder(Methods.post);
